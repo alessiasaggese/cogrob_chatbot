f52add2e518c786b43b9264946f24c9072b922e3
robertanto <a.roberto11@studenti.unisa.it>
2021-10-04 18:11:44 +0200

    dialog_management

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,36 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
-# This is a simple example for a custom action which utters "Hello World!"
+DATA = {
+    'San Francisco' : {
+        "rbry-mqwu": "Street Z, Number Y",
+        "b27b-2uc7": "Street X, Number Y",
+        "9wzi-peqs": "Street Y, Number X"
+    }
+}
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionSubmit(Action):
+
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        facility_type = tracker.get_slot("facility_type")
+        location = tracker.get_slot("location")
+
+        print("\nFacility type:",facility_type)
+        print("Location:",location)
+        
+        if location in DATA and facility_type in DATA[location]:
+            dispatcher.utter_message(text=f"This is the address: {DATA[location][facility_type]}") 
+            return [SlotSet("facility_address", DATA[location][facility_type])]
+        
+        dispatcher.utter_message(text="Sorry. I can't find this facility.") 
+
+        return []
